1e3e0bfe58b82ff687200ca8f45c84cd5189c0f2
photon <dzikrynp14@gmail.com>
2026-01-12 00:13:22 +0700

    <y>

--- internal/request/request.go
+++ internal/request/request.go
@@ -3,7 +3,6 @@ package request
 import (
 	"fmt"
 	"io"
-	"log"
 	"strings"
 )
 
@@ -20,44 +19,42 @@ type RequestLine struct {
 func RequestFromReader(reader io.Reader) (*Request, error) {
 	req, err := io.ReadAll(reader)
 	if err != nil {
-		log.Fatal("read fail")
+		return nil, fmt.Errorf("read fail")
 	}
 
-	requestLine, err := parseRequestLine(req)
+	var request Request
+	err = parseRequestLine(&request, req)
 	if err != nil {
-		log.Fatal("parse fail")
+		return nil, fmt.Errorf("parse fail")
 	}
 
-	fmt.Printf("method: %s\n", requestLine.Method)
-	fmt.Printf("destination: %s\n", requestLine.RequestTarget)
-	fmt.Printf("version: %s\n", requestLine.HTTPVersion)
-
-	return nil, nil
+	return &request, nil
 }
 
-func parseRequestLine(req []byte) (*RequestLine, error) {
-	requestLine := new(RequestLine)
+func parseRequestLine(request *Request, req []byte) error {
+	var requestLine RequestLine
 	for line := range strings.SplitSeq(string(req), "\r\n") {
 		data := strings.Split(line, " ")
 
 		if len(data) != 3 {
-			return nil, fmt.Errorf("bad request")
+			return fmt.Errorf("bad request")
 		}
 
 		if data[0] != strings.ToUpper(data[0]) {
-			return nil, fmt.Errorf("method is not capital")
+			return fmt.Errorf("method is not capital")
 		}
 
 		if data[2] != "HTTP/1.1" {
-			return nil, fmt.Errorf("version not supported")
+			return fmt.Errorf("version not supported")
 		}
 
 		requestLine.Method = data[0]
 		requestLine.RequestTarget = data[1]
 		requestLine.HTTPVersion = strings.TrimPrefix(data[2], "HTTP/")
 
+		request.RequestLine = requestLine
 		break
 	}
 
-	return requestLine, nil
+	return nil
 }

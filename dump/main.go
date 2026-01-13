3ba97798a4574599ea45db93223c44224c4d32fb
photon <dzikrynp14@gmail.com>
2026-01-13 21:41:17 +0700

    ;

--- dump/main.go
+++ dump/main.go
@@ -12,34 +12,48 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	defer f.Close()
 
-	buf := make([]byte, 8)
-	var line []byte
-	for {
-		read, err := f.Read(buf)
+	for data := range getLinesChannel(f) {
+		fmt.Printf("read: %s\n", data)
+	}
+}
 
-		if read > 0 {
-			for _, v := range buf[:read] {
-				if v != '\n' {
-					line = append(line, v)
-					continue
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	out := make(chan string)
+
+	go func() {
+		defer close(out)
+		defer f.Close()
+
+		buf := make([]byte, 8)
+		var line []byte
+		for {
+			read, err := f.Read(buf)
+
+			if read > 0 {
+				for _, v := range buf[:read] {
+					if v != '\n' {
+						line = append(line, v)
+						continue
+					}
+					if len(line) > 0 {
+						out <- string(line)
+					}
+					line = nil
 				}
+			}
+
+			if err != nil {
 				if len(line) > 0 {
-					fmt.Printf("read: %s\n", line)
+					out <- string(line)
 				}
-				line = nil
-			}
-		}
-
-		if err != nil {
-			if len(line) > 0 {
-				fmt.Printf("read: %s\n", line)
-			}
-			if errors.Is(err, io.EOF) {
+				if errors.Is(err, io.EOF) {
+					break
+				}
 				break
 			}
-			break
 		}
-	}
+	}()
+
+	return out
 }

039aaf98f20eef8487d9bf8158f8022480887699
photon <dzikrynp14@gmail.com>
2026-01-16 10:57:54 +0700

    e

---
+++ dump/headers/headers.go
@@ -0,0 +1,101 @@
+package headers
+
+import (
+	"bytes"
+	"errors"
+	"strings"
+)
+
+var (
+	ErrMalformedHeaderLine = errors.New("malformed header line")
+	ErrMalformedHeaderKey  = errors.New("malformed header key")
+	CRLF                   = []byte("\r\n")
+)
+
+type Headers map[string]string
+
+func (h Headers) Get(key string) string {
+	return h[strings.ToLower(key)]
+}
+
+func (h Headers) Set(key, value string) {
+	key = strings.ToLower(key)
+	if v, ok := h[key]; ok {
+		h[key] = v + "," + value
+		return
+	}
+	h[key] = value
+}
+
+func NewHeaders() Headers {
+	return Headers{}
+}
+
+func (h Headers) Parse(data []byte) (int, bool, error) {
+	read := 0
+	for {
+		idx := bytes.Index(data[read:], CRLF)
+		if idx == -1 {
+			break
+		}
+
+		// Empty Header
+		if idx == 0 {
+			read += len(CRLF)
+			return read, true, nil
+		}
+
+		key, value, err := parseHeader(data[read : read+idx])
+		if err != nil {
+			return read, false, err
+		}
+
+		if !validateToken([]byte(key)) {
+			return read, false, ErrMalformedHeaderKey
+		}
+
+		read += idx + len(CRLF)
+		h.Set(key, value)
+	}
+	return read, false, nil
+}
+
+func parseHeader(data []byte) (string, string, error) {
+	if idx := bytes.Index(data, []byte(":")); idx != -1 {
+		// Key not empty
+		if len(data[:idx]) == 0 {
+			return "", "", ErrMalformedHeaderLine
+		}
+
+		// No WS before colon
+		if data[idx-1] == ' ' {
+			return "", "", ErrMalformedHeaderLine
+		}
+	}
+
+	parts := bytes.SplitN(data, []byte(":"), 2)
+	for i := range parts {
+		parts[i] = bytes.TrimSpace(parts[i])
+	}
+
+	return string(parts[0]), string(parts[1]), nil
+}
+
+func validateToken(key []byte) bool {
+	for _, ch := range key {
+		found := false
+		if (ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z') || (ch >= '0' && ch <= '9') {
+			found = true
+		}
+
+		switch ch {
+		case '!', '#', '$', '%', '&', '\'', '*', '+', '-', '.', '^', '_', '`', '|', '~':
+			found = true
+		}
+
+		if !found {
+			return false
+		}
+	}
+	return true
+}
---
+++ dump/headers/headers_test.go
@@ -0,0 +1,66 @@
+package headers
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestHeaders(t *testing.T) {
+	// Test: Valid single header
+	headers := NewHeaders()
+	data := []byte("Host: localhost:42069\r\nFooFoo: barbar\r\n\r\n")
+	n, done, err := headers.Parse(data)
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	assert.Equal(t, "localhost:42069", headers.Get("Host"))
+	assert.Equal(t, "barbar", headers.Get("FooFoo"))
+	assert.Equal(t, 41, n)
+	assert.True(t, done)
+
+	// Test: Invalid spacing header
+	headers = NewHeaders()
+	data = []byte("       Host : localhost:42069       \r\n\r\n")
+	n, done, err = headers.Parse(data)
+	require.Error(t, err)
+	assert.Equal(t, 0, n)
+	assert.False(t, done)
+
+	// Test: Capital letters
+	headers = NewHeaders()
+	data = []byte("HOST: localhost:42069\r\nFooFoo: barbar\r\n\r\n")
+	n, done, err = headers.Parse(data)
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	assert.Equal(t, "localhost:42069", headers.Get("host"))
+	assert.Equal(t, "barbar", headers.Get("foofoo"))
+	assert.Equal(t, 41, n)
+	assert.True(t, done)
+
+	// Test: Invalid character header
+	headers = NewHeaders()
+	data = []byte("H©st: localhost:42069\r\n\r\n")
+	n, done, err = headers.Parse(data)
+	require.Error(t, err)
+	assert.Equal(t, 0, n)
+	assert.False(t, done)
+
+	// Test: Invalid character header
+	headers = NewHeaders()
+	data = []byte(": localhost:42069\r\n\r\n")
+	n, done, err = headers.Parse(data)
+	require.Error(t, err)
+	assert.Equal(t, 0, n)
+	assert.False(t, done)
+
+	// Test: Header key already exist
+	headers = NewHeaders()
+	data = []byte("HOST: localhost:42069\r\nHost: localhost:676767\r\n\r\n")
+	n, done, err = headers.Parse(data)
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	assert.Equal(t, "localhost:42069,localhost:676767", headers.Get("host"))
+	assert.Equal(t, 49, n)
+	assert.True(t, done)
+}
